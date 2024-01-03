dbbd4f4f107f6a14389387ae915653a2d782ee9b
MSI\Blues <truongnhlse160191@fpt.edu.vn>
2024-01-04 05:53:43 +0700

    [BE002] create entity

---
+++ src/test/java/com/whearapp/WhearAppBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.whearapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WhearAppBackEndApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
